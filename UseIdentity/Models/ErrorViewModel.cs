48b51c20158320e8077ab04c0d7a126a91c51067
Shree Ganesh Yadav <yganesh692@gmail.com>
2024-11-03 00:07:48 +0530

    Add project files.

---
+++ UseIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UseIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
